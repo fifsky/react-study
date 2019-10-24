04f71a3c6d4500eb74d3e61597a4bd1e294992e5
fifsky <fifsky@gmail.com>
2019-10-24 16:17:16 +0800

    使用react hooks

--- src/pages/List/index.js
+++ src/pages/List/index.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Table, Button, Modal, Form, Input, Popconfirm, Card } from 'antd'
-import { connect } from 'dva'
+import { useDispatch, useSelector } from 'dva'
 import { sync } from '../../util'
 
 const {TextArea} = Input
 
-@connect(({moods, loading}) => ({
-  moods,
-  loading: loading.models.moods,
-}))
-@Form.create()
-class List extends Component {
-  state = {
-    visible: false,
-    content: '',
-  }
+export default Form.create()(props => {
+  const dispatch = useDispatch()
+  const moods = useSelector(state => state.moods)
 
-  columns = [
+  const [loading, setLoading] = useState(false)
+  const [page, setPage] = useState(1)
+  const [visible, setVisible] = useState(false)
+
+  let columns = [
     {
       title: 'ID',
       dataIndex: 'id',
@@ -42,7 +39,7 @@ class List extends Component {
       render: (text, record) => {
         return (
           <Popconfirm title="确认删除?"
-                      onConfirm={() => this.onDelete(record.id)}>
+                      onConfirm={() => onDelete(record.id)}>
             <Button size="small" htmlType="button">删除</Button>
           </Popconfirm>
         )
@@ -50,53 +47,35 @@ class List extends Component {
     },
   ]
 
-  load = (page) => {
-    return this.props.dispatch({
-      type: 'moods/queryList',
-      payload: {page},
-    })
-  }
-
-  onPageChange = (page) => {
-    let self = this
+  useEffect(() => {
+    setLoading(true)
     sync(async function () {
-      await self.load(page.current)
+      await dispatch({
+        type: 'moods/queryList',
+        payload: {page},
+      })
+      setLoading(false)
     })
+  }, [page])
+
+  const onPageChange = (page) => {
+    console.log(page)
+    setPage(page.current)
   }
 
-  componentDidMount () {
-    let self = this
-    const {moods} = this.props
-
-    sync(async function() {
-      await self.load(moods.page.current)
-    })
-  }
-
-  showModal = () => {
-    this.setState({visible: true})
-  }
-
-  handleCancel = () => {
-    this.setState({
-      visible: false,
-    })
-  }
-
-  handleOk = () => {
-    const {dispatch, form: {validateFields}} = this.props
+  const handleOk = () => {
+    const {form: {validateFields}} = props
 
     validateFields((err, values) => {
       if (!err) {
-        const {form} = this.props
-        let self = this
+        const {form} = props
         sync(async function () {
           await dispatch({
             type: 'moods/add',
             payload: values,
           })
           // 重置
-          self.setState({visible: false, content: ''})
+          setVisible(false)
           form.setFieldsValue({
             content: '',
           })
@@ -105,8 +84,7 @@ class List extends Component {
     })
   }
 
-  onDelete = (id) => {
-    const {moods, dispatch} = this.props
+  const onDelete = (id) => {
     sync(async function () {
       await dispatch({
         type: 'moods/delete',
@@ -115,38 +93,178 @@ class List extends Component {
     })
   }
 
-  render () {
-    const {visible} = this.state
-    const {moods, loading} = this.props
-    const {form: {getFieldDecorator}} = this.props
+  const {form: {getFieldDecorator}} = props
 
-    return (
-      <Card
-        title="每日心情"
+  return (
+    <Card
+      title="每日心情"
+    >
+      <Button type="primary" style={{marginBottom: '10px'}}
+              onClick={() => setVisible(true)}>发表</Button>
+
+      <Table columns={columns} dataSource={moods.list} loading={loading}
+             rowKey="id" pagination={moods.page}
+             onChange={onPageChange}/>
+      <Modal
+        title="今天你😊吗？"
+        visible={visible}
+        onOk={handleOk}
+        onCancel={() => setVisible(false)}
       >
-        <Button type="primary" style={{marginBottom: '10px'}}
-                onClick={this.showModal}>发表</Button>
+        <Form>
+          {getFieldDecorator('content', {
+            rules: [{required: true}],
+          })(
+            <TextArea rows={4} autoFocus placeholder='说点什么……'/>,
+          )}
+        </Form>
+      </Modal>
+    </Card>
+  )
+})
 
-        <Table columns={this.columns} dataSource={moods.list} loading={loading}
-               rowKey="id" pagination={moods.page}
-               onChange={this.onPageChange}/>
-        <Modal
-          title="今天你😊吗？"
-          visible={visible}
-          onOk={this.handleOk}
-          onCancel={this.handleCancel}
-        >
-          <Form>
-            {getFieldDecorator('content', {
-              rules: [{required: true}],
-            })(
-              <TextArea rows={4} autoFocus placeholder='说点什么……'/>,
-            )}
-          </Form>
-        </Modal>
-      </Card>
-    )
-  }
-}
+// export default connect(({moods, loading}) => ({
+//   moods,
+//   loading: loading.models.moods,
+// }))(Form.create({})(List))
 
-export default List
+// class List extends Component {
+//   state = {
+//     visible: false,
+//     content: '',
+//   }
+//
+//   columns = [
+//     {
+//       title: 'ID',
+//       dataIndex: 'id',
+//       width: '10%',
+//     },
+//     {
+//       title: '作者',
+//       dataIndex: 'user.name',
+//       width: '12%',
+//     },
+//     {
+//       title: '心情',
+//       dataIndex: 'content',
+//     },
+//     {
+//       title: '日期',
+//       dataIndex: 'created_at',
+//       width: '30%',
+//     },
+//     {
+//       title: '操作',
+//       width: '10%',
+//       render: (text, record) => {
+//         return (
+//           <Popconfirm title="确认删除?"
+//                       onConfirm={() => this.onDelete(record.id)}>
+//             <Button size="small" htmlType="button">删除</Button>
+//           </Popconfirm>
+//         )
+//       },
+//     },
+//   ]
+//
+//   load = (page) => {
+//     return this.props.dispatch({
+//       type: 'moods/queryList',
+//       payload: {page},
+//     })
+//   }
+//
+//   onPageChange = (page) => {
+//     let self = this
+//     sync(async function () {
+//       await self.load(page.current)
+//     })
+//   }
+//
+//   componentDidMount () {
+//     let self = this
+//     const {moods} = this.props
+//
+//     sync(async function() {
+//       await self.load(moods.page.current)
+//     })
+//   }
+//
+//   showModal = () => {
+//     this.setState({visible: true})
+//   }
+//
+//   handleCancel = () => {
+//     this.setState({
+//       visible: false,
+//     })
+//   }
+//
+//   handleOk = () => {
+//     const {dispatch, form: {validateFields}} = this.props
+//
+//     validateFields((err, values) => {
+//       if (!err) {
+//         const {form} = this.props
+//         let self = this
+//         sync(async function () {
+//           await dispatch({
+//             type: 'moods/add',
+//             payload: values,
+//           })
+//           // 重置
+//           self.setState({visible: false, content: ''})
+//           form.setFieldsValue({
+//             content: '',
+//           })
+//         })
+//       }
+//     })
+//   }
+//
+//   onDelete = (id) => {
+//     const {moods, dispatch} = this.props
+//     sync(async function () {
+//       await dispatch({
+//         type: 'moods/delete',
+//         payload: {id: id, page: moods.page.current},
+//       })
+//     })
+//   }
+//
+//   render () {
+//     const {visible} = this.state
+//     const {moods, loading} = this.props
+//     const {form: {getFieldDecorator}} = this.props
+//
+//     return (
+//       <Card
+//         title="每日心情"
+//       >
+//         <Button type="primary" style={{marginBottom: '10px'}}
+//                 onClick={this.showModal}>发表</Button>
+//
+//         <Table columns={this.columns} dataSource={moods.list} loading={loading}
+//                rowKey="id" pagination={moods.page}
+//                onChange={this.onPageChange}/>
+//         <Modal
+//           title="今天你😊吗？"
+//           visible={visible}
+//           onOk={this.handleOk}
+//           onCancel={this.handleCancel}
+//         >
+//           <Form>
+//             {getFieldDecorator('content', {
+//               rules: [{required: true}],
+//             })(
+//               <TextArea rows={4} autoFocus placeholder='说点什么……'/>,
+//             )}
+//           </Form>
+//         </Modal>
+//       </Card>
+//     )
+//   }
+// }
+//
+// export default List
